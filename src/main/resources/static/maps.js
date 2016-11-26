0298ae5901ffad83253e9af9eb1b6552f6847dff
Johannes Teklote <johannes.teklote@web.de>
2016-11-26 18:59:26 +0100

    map with hardcoded data added

---
+++ src/main/resources/static/maps.js
@@ -0,0 +1,372 @@
+/**
+ * Created by Johannes Teklote on 26.11.2016.
+ */
+var data = [
+    {
+        "type": "node",
+        "id": 251337995,
+        "lat": 60.1716226,
+        "lon": 24.9447123,
+        "tags": {
+            "amenity": "toilets",
+            "created_by": "Potlatch 0.7b"
+        }
+    },
+    {
+        "type": "node",
+        "id": 327313510,
+        "lat": 60.1751353,
+        "lon": 24.9803124,
+        "tags": {
+            "amenity": "toilets"
+        }
+    },
+    {
+        "type": "node",
+        "id": 600408141,
+        "lat": 60.1756736,
+        "lon": 24.9495482,
+        "tags": {
+            "amenity": "toilets",
+            "fee": "male pee = free, other side 0,5 &euro;"
+        }
+    },
+    {
+        "type": "node",
+        "id": 603743745,
+        "lat": 60.1684109,
+        "lon": 24.9520396,
+        "tags": {
+            "addr:city": "Helsinki",
+            "addr:country": "FI",
+            "addr:postcode": "00170",
+            "addr:street": "Sofiankatu",
+            "amenity": "toilets",
+            "opening_hours": "Mo-Su 09:30-16:00",
+            "operator": "Helsingin kaupunki"
+        }
+    },
+    {
+        "type": "node",
+        "id": 606920080,
+        "lat": 60.1517502,
+        "lon": 24.9571746,
+        "tags": {
+            "addr:city": "Helsinki",
+            "addr:country": "FI",
+            "amenity": "toilets",
+            "fee": "no",
+            "name": "Harakka"
+        }
+    },
+    {
+        "type": "node",
+        "id": 736394575,
+        "lat": 60.1742126,
+        "lon": 24.9885572,
+        "tags": {
+            "amenity": "toilets"
+        }
+    },
+    {
+        "type": "node",
+        "id": 736394593,
+        "lat": 60.1750076,
+        "lon": 24.9885714,
+        "tags": {
+            "amenity": "toilets"
+        }
+    },
+    {
+        "type": "node",
+        "id": 1376356036,
+        "lat": 60.1731955,
+        "lon": 24.9496649,
+        "tags": {
+            "amenity": "toilets"
+        }
+    },
+    {
+        "type": "node",
+        "id": 1872483992,
+        "lat": 60.1666710,
+        "lon": 24.9528575,
+        "tags": {
+            "amenity": "toilets",
+            "wheelchair": "yes"
+        }
+    },
+    {
+        "type": "node",
+        "id": 2023969771,
+        "lat": 60.1750434,
+        "lon": 24.9820533,
+        "tags": {
+            "amenity": "toilets"
+        }
+    },
+    {
+        "type": "node",
+        "id": 2389029155,
+        "lat": 60.1746831,
+        "lon": 24.9838100,
+        "tags": {
+            "amenity": "toilets",
+            "fee": "no"
+        }
+    },
+    {
+        "type": "node",
+        "id": 2451481435,
+        "lat": 60.1548299,
+        "lon": 24.9549527,
+        "tags": {
+            "amenity": "toilets"
+        }
+    },
+    {
+        "type": "node",
+        "id": 3926353226,
+        "lat": 60.1537670,
+        "lon": 24.9560053,
+        "tags": {
+            "access": "public",
+            "amenity": "toilets",
+            "female": "yes",
+            "male": "yes",
+            "seasonal:summer": "yes",
+            "seasonal:winter": "no"
+        }
+    },
+    {
+        "type": "way",
+        "id": 245174939,
+        "nodes": [
+            2524147447,
+            2524147446,
+            2524147444,
+            2524147445,
+            2524147447
+        ],
+        "tags": {
+            "amenity": "toilets",
+            "building": "yes",
+            "fee": "0,5 &euro;",
+            "fee:male:conditional": "none @ urinate",
+            "wheelchair": "yes"
+        }
+    },
+    {
+        "type": "way",
+        "id": 308779871,
+        "nodes": [
+            3140499512,
+            3140499509,
+            3140499507,
+            3140499511,
+            3140499512
+        ],
+        "tags": {
+            "amenity": "toilets",
+            "building": "yes",
+            "fee": "male pee = free, other side 0,5 &euro;"
+        }
+    },
+    {
+        "type": "way",
+        "id": 326441953,
+        "nodes": [
+            3330871438,
+            3330871435,
+            3330871436,
+            3330871440,
+            3330871438
+        ],
+        "tags": {
+            "amenity": "toilets",
+            "building": "yes"
+        }
+    },
+    {
+        "type": "way",
+        "id": 368507884,
+        "nodes": [
+            3723667908,
+            3723667909,
+            3723667906,
+            3723667905,
+            3723667908
+        ],
+        "tags": {
+            "amenity": "toilets",
+            "building": "yes",
+            "fee": "no"
+        }
+    },
+    {
+        "type": "node",
+        "id": 3723667905,
+        "lat": 60.1543061,
+        "lon": 24.9425508
+    },
+    {
+        "type": "node",
+        "id": 3723667906,
+        "lat": 60.1543096,
+        "lon": 24.9426141
+    },
+    {
+        "type": "node",
+        "id": 3723667908,
+        "lat": 60.1543247,
+        "lon": 24.9425466
+    },
+    {
+        "type": "node",
+        "id": 3723667909,
+        "lat": 60.1543282,
+        "lon": 24.9426100
+    },
+    {
+        "type": "node",
+        "id": 2524147444,
+        "lat": 60.1631585,
+        "lon": 24.9452783
+    },
+    {
+        "type": "node",
+        "id": 2524147445,
+        "lat": 60.1631718,
+        "lon": 24.9453230
+    },
+    {
+        "type": "node",
+        "id": 2524147446,
+        "lat": 60.1631757,
+        "lon": 24.9452576
+    },
+    {
+        "type": "node",
+        "id": 2524147447,
+        "lat": 60.1631890,
+        "lon": 24.9453022
+    },
+    {
+        "type": "node",
+        "id": 3330871435,
+        "lat": 60.1632884,
+        "lon": 24.9450598
+    },
+    {
+        "type": "node",
+        "id": 3330871436,
+        "lat": 60.1633048,
+        "lon": 24.9451115
+    },
+    {
+        "type": "node",
+        "id": 3330871438,
+        "lat": 60.1633102,
+        "lon": 24.9450318
+    },
+    {
+        "type": "node",
+        "id": 3330871440,
+        "lat": 60.1633266,
+        "lon": 24.9450835
+    },
+    {
+        "type": "node",
+        "id": 3140499507,
+        "lat": 60.1736633,
+        "lon": 24.9611992
+    },
+    {
+        "type": "node",
+        "id": 3140499509,
+        "lat": 60.1736710,
+        "lon": 24.9611447
+    },
+    {
+        "type": "node",
+        "id": 3140499511,
+        "lat": 60.1737111,
+        "lon": 24.9612264
+    },
+    {
+        "type": "node",
+        "id": 3140499512,
+        "lat": 60.1737188,
+        "lon": 24.9611719
+    }
+];
+
+var prev;
+var prevWindow;
+var map;
+
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(60.16522291873824, 24.968039989471436),
+        zoom: 15/*,
+        disableDefaultUI: true,
+        draggable: false,
+        keyboardShortcuts: false,
+        rotateControl: false,
+        scrollwheel: false*/
+    };
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    for (var i = 0; i < data.length; i++) {
+        var marker = new google.maps.Marker({
+            id: i,
+            position: new google.maps.LatLng(data[i].lat, data[i].lon),
+            icon: 'marker_red.png'
+        });
+
+        marker.setMap(map);
+
+        google.maps.event.addListener(marker, 'click', function(event) {
+            if (prev != undefined) {
+                prev.setIcon('marker_red.png');
+            }
+            if (prevWindow != undefined) {
+                prevWindow.close();
+            }
+            this.setIcon('marker_blue.png');
+
+            var infowindow = new google.maps.InfoWindow({
+                content: createContent(data, this.id)
+            });
+            infowindow.open(map, this);
+            prevWindow = infowindow;
+            prev = this;
+        });
+    }
+}
+
+function createContent(data, index) {
+    var out = "";
+    if (data[index].tags == undefined) {
+        return "no detailed information avialable";
+    }
+    if (data[index].tags.charge != undefined) {
+        out += "charge: " + data[index].tags.charge + " &euro;\<br>";
+    }
+    if (data[index].tags.fee != undefined) {
+        out += "fee: " + data[index].tags.fee + "\<br>";
+    }
+    if (data[index].tags.operator != undefined) {
+        out += "operator: " + data[index].tags.operator + "\<br>";
+    }
+    if (data[index].tags.opening_hours != undefined) {
+        out += "opening hours: " + data[index].tags.opening_hours + " h\<br>";
+    }
+    if (data[index].tags.unisex != undefined) {
+        out += "unisex: " + data[index].tags.unisex + "\<br>";
+    }
+    if (out == "") {
+        out += "no detailed information avialable.";
+    }
+    return out;
+}
