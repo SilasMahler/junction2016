f9ce06d0f1474e6e92bbbe9bc197e113a4dbc244
Silas Mahler <silas.mahler@fh-dortmund.de>
2016-11-27 04:09:30 +0200

    Added some thing

---
+++ src/main/resources/static/sites.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Silas on 27.11.2016.
+ */
+
+var sites = ["enterPassword", "events", "explosion", "jokes", "map", "news", "numbers", "quiz", "someAdd"];
+
+function getSites(){
+    return sites;
+}
